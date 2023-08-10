a83bef2e2e36a7a585bce6dc5b64615031201305
YsN23 <yy2369964@gmail.com>
2023-08-10 06:37:55 -0700

    Done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,11 +17,10 @@ long int N = 164724245;
 long long int R = 2137108271611771;
 float pi = 3.142316;
 
-printf("Size of a char: %d byte (s) \n", sizeof(a));
-printf("Size of an int: %d byte (s) \n", sizeof(num));
-printf("Size of a long int: %ld byte (s)\n", sizeof(N));
-printf("Size of a long long int: %ld byte (s)\n", sizeof(R));
-printf("Size of a float: %d byte (s)\n", sizeof(pi));
-
-return 0;
+printf("Size of a char: %d byte(s)\n", sizeof(a));
+printf("Size of an int: %d byte(s)\n", sizeof(num));
+printf("Size of a long int: %ld byte(s)\n", sizeof(N));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(R));
+printf("Size of a float: %d byte(s)\n", sizeof(pi));
+return (0);
 }
