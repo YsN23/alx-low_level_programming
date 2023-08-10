d9209e6b213b6ab466ac37c24c3f285eae8eb94a
YsN23 <yy2369964@gmail.com>
2023-08-10 02:45:57 -0700

    Done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,13 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-        char text[] = "\"Programming is like building a multilingual puzzle";
-        puts(text);
-        return (0);
+
+char text[] = "\"Programming is like building a multilingual puzzle";
+
+puts(text);
+return (0);
+
 }
