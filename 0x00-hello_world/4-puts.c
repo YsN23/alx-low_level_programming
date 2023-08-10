3628539e517be418f9b5c9a7ac46fc2f13d244d8
YsN23 <yy2369964@gmail.com>
2023-08-10 02:32:53 -0700

    Done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * This Program will print a text
+ *-include the standard i/o library
+ *-use main function
+ *-Declare a char var contain the text
+ *-use puts function
+ *
+ */
+
+int main(void)
+{
+	char text[] = "\"Programming is like building a multilingual puzzle";
+	puts(text);
+	return 0;
+}
