14ca69ee7fe26fd69091cea55507e0c21ee0f32e
YsN23 <yy2369964@gmail.com>
2023-08-11 09:14:38 -0700

    Done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers base 10.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i = 0;
+
+while (i < 10)
+{
+	printf("%d", i);
+	i++;
+}
+printf("\n");
+return (0);
+}
