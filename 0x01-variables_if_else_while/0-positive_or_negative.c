f8bb3564f06204cd0e7ac36131ce9a2fac1b61f1
YsN23 <yy2369964@gmail.com>
2023-08-11 07:13:21 -0700

    Done

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - generate random int number, is it posetive, negative, or equal to 0 ?
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 int n;
