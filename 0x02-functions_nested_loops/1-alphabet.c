93c06077b79b6f33cdb1dc9cff25d1b0f35ff451
YsN23 <yy2369964@gmail.com>
2023-08-16 07:33:25 -0700

    Done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * print_alphabet - use putchar function to print a to z in lower.
+ *
+ */
+
+void print_alphabet(void)
+{
+char lowercaseAlphabet = 'a';
+
+while (lowercaseAlphabet <= 'z')
+{
+	_putchar(lowercaseAlphabet);
+	lowercaseAlphabet++;
+
+}
+_putchar('\n');
+}
