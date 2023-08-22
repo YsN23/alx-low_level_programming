fbb43be16346bb39768c151f8400b014cce825b8
YsN23 <yy2369964@gmail.com>
2023-08-22 03:46:50 -0700

    Done

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates a random password
+ * 
+ * Return: Always 0 (Success)
+*/
+
+int main(void) {
+    int length = 14;
+    char password[length + 1];
+    char charset[] = "abcdefghijklmnopqrstuvwxyz%!&$*@#^ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    int charset_size = sizeof(charset) - 1;
+
+    srand(time(NULL));
+    for (int i = 0; i < length; i++) {
+        int index = rand() % charset_size;
+        password[i] = charset[index];
+    }
+    password[length] = '\0';
+
+    printf("%s\n", password);
+    return 0;
+}
