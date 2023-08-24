22d522510d1689465b8d16444d9895649b3ab2c6
root <root@DESKTOP-KBSVJN4.localdomain>
2023-08-24 13:14:37 +0100

    Done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,15 +13,14 @@ char *_strcat(char *dest, char *src)
 {
 int i, j;
 
-for (i = 0; dest[i] != '\0'; i++)
+while (dest[i])
 {
-	continue;
-}
-for (j = 0; src[j] != '\0'; j++)
-{
-	dest[i] = src[j];
 	i++;
 }
-dest[i] = '\0';
+
+for (j = 0; src[j] ; j++)
+{
+	dest[i++] = src[j];
+}
 return (dest);
 }
