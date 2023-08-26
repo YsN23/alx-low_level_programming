f368c00fe8a6d7294022db2d1b428357964b8f2e
root <root@DESKTOP-KBSVJN4.localdomain>
2023-08-26 01:36:35 +0100

    Done

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * *rot13 - encode
+ *
+ * @str: str param
+ *
+ * Return: str
+ */
+
+char *rot13(char *str)
+{
+	int i = 0;
+	int j = 0;
+	char *p1 = "ABCDEFGHIJKLMabcdefghijklm";
+	char *p2 = "NOPQRSTUVWXYZnopqrstuvwxyz";
+
+	while (str[i] != '\0')
+	{
+	j = 0;
+	while (j != '\0')
+	{
+	if (str[i] == p1[j])
+	{
+	str[i] = p2[j];
+	break;
+	}
+	j++;
+	}
+
+	i++;
+	}
+
+	return (str);
+}
