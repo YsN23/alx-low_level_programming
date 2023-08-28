d84643e4fbca7d5ef3749998ade262d15848c9c7
root <root@DESKTOP-KBSVJN4.localdomain>
2023-08-28 18:18:35 +0100

    Done

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: Pointer Point to string
+ * @accept: Pointer Point To string
+ * Return: result
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+int result = 0;
+int i;
+
+while (s[i] != '\0')
+{
+if (s[i] == accept)
+{
+	result += 1;
+}
+
+i++;
+}
+
+return (result);
+
+}
