3eb0a482126480be9acd40f6b5a5734ac681e33b
root <root@DESKTOP-KBSVJN4.localdomain>
2023-08-28 12:29:56 +0100

    Done

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - find the first occurence c in s
+ * @s: Pointer Point To string
+ * @c: char to find
+ * Return: s[c] (Success) otherwise NULL
+*/
+
+char *_strchr(char *s, char c)
+{
+int i;
+
+while (s[i] != '\0')
+{
+if (s[i] == c)
+{
+	return (s);
+}
+
+s++;
+}
+return ("NULL");
+
+}
