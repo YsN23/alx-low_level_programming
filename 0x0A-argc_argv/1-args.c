c76d1c6424a50f22ccf7022088ea2728539d3f0c
root <root@DESKTOP-KBSVJN4.localdomain>
2023-09-02 01:28:39 +0100

    Done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print The numbers of arguments in CL
+ * @argc: integer
+ * @argv: pointer to array of string
+ * Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
