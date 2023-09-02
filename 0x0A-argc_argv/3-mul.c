e53616aa50c4a2fc79b40df91dfcbff276514fd1
root <root@DESKTOP-KBSVJN4.localdomain>
2023-09-02 01:51:29 +0100

    Done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,30 +2,33 @@
 #include <stdlib.h>
 
 /**
-* main - Print the product of two integers passed as command-line arguments
-* @argc: integer (argument count)
-* @argv: array of strings (argument values)
-* Return: Always 0 (Success)
-*/
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the arguments.
+ *
+ * Return: 0 for success, 1 for error.
+ */
 int main(int argc, char *argv[])
 {
-	int mul = 1;
-	int num1;
-	int num2;
+	int i = 1;
+	int multi = 1;
 
-	if (argc != 3)
+	/* Check if there are exactly two arguments */
+	if (argc == 3)
 	{
-		printf("ERROR\n");
-		return (1);
+	/* Calculate the product of the two arguments */
+	while (i < argc)
+	{
+	multi *= atoi(argv[i]);
+	i++;
 	}
-
-	num1 = atoi(argv[1]);
-
-	num2 = atoi(argv[2]);
-
-	mul = num1 * num2;
-
-	printf("%d\n", mul);
-
+	printf("%d\n", multi);
+	}
+	else
+	{
+	/* Print an error message and return 1 for error */
+	printf("Error\n");
+	return (1);
+	}
 	return (0);
 }
