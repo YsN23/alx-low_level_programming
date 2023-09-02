6f0b0878e68f5e68d2390496ee733e29051ebfa6
root <root@DESKTOP-KBSVJN4.localdomain>
2023-09-02 01:47:12 +0100

    Done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Print the product of two integers passed as command-line arguments
+* @argc: integer (argument count)
+* @argv: array of strings (argument values)
+* Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	int mul = 0;
+	int num1;
+	int num2;
+
+	if (argc != 3)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+
+	num2 = atoi(argv[2]);
+
+	mul = num1 * num2;
+
+	printf("%d\n", mul);
+
+	return (0);
+}
