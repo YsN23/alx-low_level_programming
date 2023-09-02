88a0ae9048260ed80df75a21dc6f982b41f53259
root <root@DESKTOP-KBSVJN4.localdomain>
2023-09-02 01:17:29 +0100

    Done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the program name
+ * @argc: integer
+ * @argv: pointer to array of string
+ * Return: Always 0 (Success)
+ *
+*/
+
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
