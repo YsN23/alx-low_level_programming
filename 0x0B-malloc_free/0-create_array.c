ea52c64340d12989aa61324dfa083ce1c51d88c0
root <root@DESKTOP-KBSVJN4.localdomain>
2023-09-04 13:05:55 +0100

    Done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,30 +2,30 @@
 #include "main.h"
 
 /**
- * create_array -  creates an array of chars
- * @size: int size of chars
- * @c: char to print
- * Return: char
+* create_array - creates an array of chars and initializes with char
+* @size: size of the array to create
+* @c: character to initialize the array elements
+* Return: a pointer to the created array, or NULL on failure
 */
-
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+
+	char *ptr;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
 
-	int *ptr;
-	int i;
-
-	ptr = (int *) malloc(size * sizeof(char));
+	ptr = (char *)malloc(size * sizeof(char));
 
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
 	}
