c6c7345b004ee623277590d35f9b028f70a82eb9
root <root@DESKTOP-KBSVJN4.localdomain>
2023-09-05 10:05:09 +0100

    Done

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * free_grid - Frees memory allocated for a 2D grid.
+ * @grid: Grid to be freed.
+ * @height: Height of the grid.
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
