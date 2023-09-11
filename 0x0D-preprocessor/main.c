911d39c38dfbab278a8314ea9b664cdbfbd615a7
root <root@DESKTOP-KBSVJN4.localdomain>
2023-09-11 20:46:22 +0100

    Done

--- 0x0D-preprocessor/main.c
+++ 0x0D-preprocessor/main.c
@@ -1,6 +1,6 @@
-#include "1-pi.h"
-#include "1-pi.h"
 #include <stdio.h>
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h"
 
 /**
 * main - check the code
@@ -9,12 +9,12 @@
 */
 int main(void)
 {
-	float a;
+	int i;
 
-	float r;
+	int j;
 
-	r = 98;
-	a = PI * r * r;
-	printf("%.3f\n", a);
+	i = ABS(-98) * 10;
+	j = ABS(98) * 10;
+	printf("%d, %d\n", i, j);
 	return (0);
 }
