99bec59762de3735675fc258237ff772611e06e5
root <root@DESKTOP-KBSVJN4.localdomain>
2023-09-11 13:12:20 +0100

    Done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,14 +11,13 @@
  * This structure is used to store information about a dog,
  * including its name, age, and owner.
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
