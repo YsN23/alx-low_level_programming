3eadb88ef1e1011853827211e3c64b47b4f87c2a
root <root@DESKTOP-KBSVJN4.localdomain>
2023-09-12 19:28:38 +0100

    Done

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
 #ifndef FUNCTIONS_POINTERS
 #define FUNCTIONS_POINTERS
-
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char c);
 #endif
