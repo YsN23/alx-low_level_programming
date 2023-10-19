ef58fd8354904e61d556716f69b0640be764adb5
YsN23 <yy2369964@gmail.com>
2023-10-19 19:25:48 +0100

    Done

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* list_len - l3adab ach ghadir
+* @h: Pointer To Nodes
+* Return: Num Nodes
+*/
+
+size_t list_len(const list_t *h)
+{
+	int num_ = 0;
+
+	while (h != NULL)
+	{
+		num_ += 1;
+		h = h->next;
+	}
+
+	return (num_);
+
+}
