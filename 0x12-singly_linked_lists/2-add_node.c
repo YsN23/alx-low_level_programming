9261553e63270d48dc9c5c197759fd1edf3dd84d
YsN23 <yy2369964@gmail.com>
2023-10-19 19:38:15 +0100

    Done

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+* add_node - l3adab mn jadid
+* @head: Pointer to a pointer to the head of the linked list.
+* @str: Pointer to the string to be added.
+*
+* Return: Address of the new element, or NULL if it failed.
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *n_node = malloc(sizeof(list_t));
+
+	if (n_node == NULL)
+		return (NULL);
+
+	n_node->str = strdup(str);
+	n_node->len = strlen(str);
+	n_node->next = *head;
+	*head = n_node;
+
+
+	return (n_node);
+}
