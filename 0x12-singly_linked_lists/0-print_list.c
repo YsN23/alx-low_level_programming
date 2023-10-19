df6e8157c30ca528d31bc3b4d5f9ea3891d6a683
YsN23 <yy2369964@gmail.com>
2023-10-19 19:32:56 +0100

    Done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,23 +8,23 @@
 
 size_t print_list(const list_t *h)
 {
-		int num_nodes = 0;
+	int num_nodes = 0;
 
-		while (h != NULL)
+	while (h != NULL)
+	{
+		if (h->str == NULL)
 		{
-			if (h->str == NULL)
-			{
-				printf("[0] (nil)\n");
-			}
-
-			else
-			{
-				printf("[%u] %s\n", h->len, h->str);
-			}
-
-			h = h->next;
-			num_nodes += 1;
+			printf("[0] (nil)\n");
 		}
-		return (num_nodes);
+
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+
+		h = h->next;
+		num_nodes += 1;
+	}
+	return (num_nodes);
 
 }
