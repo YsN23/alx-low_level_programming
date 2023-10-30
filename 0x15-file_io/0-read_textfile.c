84fb645fe4980ac169d3b9166ee08b029da3db8b
YsN23 <yy2369964@gmail.com>
2023-10-30 21:51:37 +0100

    Done

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and writes
+ * its contents to standard output.
+ * @filename: The name of the file to read.
+ * @letters: The maximum number of letters to read and write.
+ * Return: The actual number of letters read and written
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t _open, _read, _write;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	_open = open(filename, O_RDONLY);
+	_read = read(_open, buffer, letters);
+	_write = write(STDOUT_FILENO, buffer, read);
+
+	if (open == -1 || read == -1 || write == -1 || write != read)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(_open);
+
+	return (_write);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+open close the f*cking file
