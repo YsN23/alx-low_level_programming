cccce8ba989e14920a48aebec1e53639fb794f67
YsN23 <yy2369964@gmail.com>
2023-12-10 20:19:02 +0100

    Done

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds a node in the end of dll
+ * @head: a double pointer to the head of dll
+ * @n: data int
+ * Return: new node
+*/
+
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *curr;
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (!head)
+	{
+		*head = new_node;
+	}
+
+	else
+	{
+		curr = *head;
+
+		while (curr != NULL)
+		{
+			curr = curr->next;
+		}
+
+		curr->next = new_node;
+		new_node->prev = curr;
+
+	}
+
+	return (new_node);
+}
