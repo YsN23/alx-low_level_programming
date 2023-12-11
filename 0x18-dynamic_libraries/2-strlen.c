4acc83e29c855602173d7ded03ac43e40763466a
YsN23 <yy2369964@gmail.com>
2023-12-11 20:29:56 +0100

    Task 0 Done

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Calculate the lenght of a string.
+ * @s: Pointer that point to a string
+ * Return: Lenght of Str
+*/
+
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+
+}
+return (i);
+}
