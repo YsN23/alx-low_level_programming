8a262aaa0777f4e5be5145cec97cf6991bc766d3
YsN23 <yy2369964@gmail.com>
2023-12-27 00:11:04 +0100

    Done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,12 +8,30 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table = malloc(sizeof(size));
+	unsigned int i = 0;
+
+	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
 
 	if (hash_table == NULL)
 	{
 		return (NULL);
 	}
+
+	hash_table->size = size;
+	hash_table->array = calloc(size, sizeof(hash_table_t *));
+
+	if (hash_table->array == NULL)
+	{
+		return (NULL);
+	}
+
+	while (i < size)
+	{
+		hash_table->array[i] = NULL;
+
+		i++;
+	}
+
 
 	return (hash_table);
 }
